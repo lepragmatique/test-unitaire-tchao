aa563a29bebc475ceaed1c1fabe6c4735e7db78a
Tchao Franki <joelxavif@gmail.com>
2023-05-16 12:22:26 +0200

    creation package main et class calculator

---
+++ maven-tchao/src/main/java/be/eafcuccle/projint/maven/tchao/main/Calculator.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package be.eafcuccle.projint.maven.tchao.main;
+
+/**
+ *
+ * @author joelx
+ */
+public class Calculator {
+
+    public Calculator() {
+    }
+    
+    public int somme(int a, int b){
+        
+        return a+b;
+    }
+    
+    public int soustraction(int a, int b){
+        return a-b;
+    }
+    
+}
