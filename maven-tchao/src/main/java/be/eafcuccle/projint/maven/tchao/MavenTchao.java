78ccbd8263b09b37aec1869978d6346d9579fb08
Tchao Franki <joelxavif@gmail.com>
2023-05-16 11:56:12 +0200

    creation du dossier maven et gestion de dépendence

---
+++ maven-tchao/src/main/java/be/eafcuccle/projint/maven/tchao/MavenTchao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package be.eafcuccle.projint.maven.tchao;
+
+/**
+ *
+ * @author joelx
+ */
+public class MavenTchao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
